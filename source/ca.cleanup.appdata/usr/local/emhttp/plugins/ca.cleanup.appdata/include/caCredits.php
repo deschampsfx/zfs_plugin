6584e5338893afa095b8c01e5351ebfadb2aaba0
Squidly271 <azawadzki@arrowgames.com>
2019-08-18 15:25:04 -0400

    add another cya

--- source/ca.cleanup.appdata/usr/local/emhttp/plugins/ca.cleanup.appdata/include/caCredits.php
+++ source/ca.cleanup.appdata/usr/local/emhttp/plugins/ca.cleanup.appdata/include/caCredits.php
@@ -5,22 +5,6 @@
 #                                                             #
 ###############################################################
 
-function getLineCount($directory) {
-  global $lineCount;
-
-  $allFiles = array_diff(scandir($directory),array(".",".."));
-  foreach ($allFiles as $file) {
-    if (is_dir("$directory/$file")) {
-      getLineCount("$directory/$file");
-      continue;
-    }
-    $extension = pathinfo("$directory/$file",PATHINFO_EXTENSION);
-    if ( $extension == "sh" || $extension == "php" || $extension == "page" ) {
-      $lineCount = $lineCount + count(file("$directory/$file"));
-    }
-  }
-}
-
 $caCredits = "
     <center><table align:'center' style='background-color:initial;'>
       <tr>
@@ -34,7 +18,6 @@ $caCredits = "
     <center><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7M7CBCVU732XG' target='_blank'><img src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'></a></center>
     <br><center><a href='http://lime-technology.com/forum/index.php?topic=40262.0' target='_blank'>Plugin Support Thread</a></center>
   ";
-  getLineCount("/usr/local/emhttp/plugins/ca.cleanup.appdata");
-  $caCredits .= "<center>$lineCount Lines of code and counting!</center>";
+
   $caCredits = str_replace("\n","",$caCredits);
 ?>
